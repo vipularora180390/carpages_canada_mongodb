e8e8ad95ca1269e0fcbe03441c50cd00fdca37f4
vipularora180390 <vipularora180390@gmail.com>
2023-06-27 16:18:35 +0530

    HashRouter

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
-import {  BrowserRouter as Router} from 'react-router-dom';
+import {  HashRouter as Router} from 'react-router-dom';
 import Sidebar from './MyComponents/Sidebar';
 import myStore ,{PersistStore} from './store';
 import reportWebVitals from './reportWebVitals';
